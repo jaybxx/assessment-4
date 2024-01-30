30e9a000dd5d127f4d15cdcb5465f73c92b1914d
Jamal Buckley <jaybxx94@gmail.com>
2024-01-30 06:00:36 -0800

    tough

--- client/main.js
+++ client/main.js
@@ -1,4 +1,5 @@
 const complimentBtn = document.querySelector("#complimentButton")
+const fortuneBtn = document.querySelector("#fortuneButton")
 
 const getCompliment = () => {
     axios.get("http://localhost:4000/api/compliment/")
@@ -8,4 +9,14 @@ const getCompliment = () => {
     });
 };
 
+const getFortune = () => {
+    axios.get("http://localhost:4000/api/fortune/")
+        .then(res => {
+            const data = res.data;
+            alert(data);
+    });
+};
+
+
+fortuneBtn.addEventListener('click', getFortune)
 complimentBtn.addEventListener('click', getCompliment)
--- server/controller.js
+++ server/controller.js
@@ -8,6 +8,15 @@ module.exports = {
         let randomCompliment = compliments[randomIndex];
       
         res.status(200).send(randomCompliment);
-    }
+    },
 
+    getFortune: (req, res) => {
+        const fortunes = ["A dubious friend may be an enemy in camouflage.", "A fresh start will put you on your way.", "People find it difficult to resist your persuasive manner.", "Romance moves you in a new direction.", "You are almost there."];
+      
+        
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+      
+        res.status(200).send(randomFortune);
+    }
 }
